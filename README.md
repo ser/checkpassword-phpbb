afc3cf3abeacd8632de3e80aa959eb78a22fbeb7
Serge Victor <github@random.re>
2014-08-27 17:23:25 +0700

    first release, tested on dovecot.

--- README.md
+++ README.md
@@ -2,3 +2,22 @@ checkpassword-phpbb
 ===================
 
 DJB-compatible checkpassword for phpBB MySQL user database, used for Dovecot IMAP
+
+The interface is specified in http://cr.yp.to/checkpwd/interface.html 
+
+Dovecot related manual: http://wiki2.dovecot.org/AuthDatabase/CheckPassword
+
+Debugging IMAP server:
+
+$ python3
+>>> import imaplib
+>>> mail = imaplib.IMAP4_SSL('imap.server.com')
+>>> mail.login('fiutek@gov.pl', 'password')
+
+and check /var/log/mail.log on the server side.
+
+Author
+======
+
+Serge Victor, https://random.re/ 
+LGPLv3, 2014
---
+++ checkpassword-phpbb.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+#
+# (C) Serge Victor, LGPLv3 license
+# https://github.com/ser/checkpassword-phpbb
+#
+# the easiest way for debugging in a command line:
+# printf "%s\0%s\0%s\0" unknown bloggs Y123456 | ./checkpassword-phpbb.py id 3<&0
+#
+import os
+import pwd
+import sys
+import traceback
+import mysql.connector
+from passlib.hash import phpass # https://pythonhosted.org/passlib/lib/passlib.hash.phpass.html
+
+# phpBB MySQL details
+config = {
+  'user': '',
+  'password': '',
+  'host': '',
+  'database': '',
+  'raise_on_warnings': True,
+}
+
+def verifypass(username,password):
+    # Connect to the MySQL Server
+    cnx = mysql.connector.connect(**config)
+    # we want to ask mysql for password for particula phpBB user
+    sql = "SELECT user_password AS password FROM phpbb_users WHERE username_clean = '%s'" % username
+    cursor = cnx.cursor(buffered=True)
+    cursor.execute(sql)
+    row = cursor.fetchone() # we need one record only
+    cursor.close()
+    cnx.close()
+    if row is None: # no such user, it cannot be authenticated   
+       return False
+    phpbbppasshash = bytes.decode(row[0]) 
+    return phpass.verify(password, phpbbppasshash) # True = user verified
+
+def main():
+    scriptname = os.path.basename(sys.argv[0])
+    with os.fdopen(3) as infile:
+        data = infile.read(512).split('\0')
+    username, password = data[:2]
+    justuser = username.split("@")[0]
+    domain = username.split("@")[1]
+    print(justuser)
+    if verifypass(justuser,password) == False:
+       return 1 # password does not match, user cannot be authenticated
+    else:
+       os.environ['USER'] = justuser
+       os.environ['HOME'] = "/home/vmail/%s/%s" % (domain, justuser) # adapt it to your needs
+       os.environ['userdb_uid'] = "999" # adapt it to your needs
+       os.environ['userdb_gid'] = "999" # adapt it to your needs
+       os.environ['userdb_mail'] = "maildir:~/Maildir" # adapt it to your needs
+       os.environ['INSECURE_SETUID'] = "1"
+       os.environ['EXTRA'] = 'userdb_uid userdb_gid userdb_mail'
+    os.execvp(sys.argv[1], sys.argv[1:])
+    # bye bye ;-)
+
+try:
+    sys.exit(main() or 111)
+except KeyboardInterrupt:
+    sys.exit(2)
+except Exception:
+    traceback.print_exc(file=sys.stderr)
+    sys.exit(111)
