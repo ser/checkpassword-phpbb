4d6e5e81cffa0550902b5dfaaa607911f87701d7
Serge Victor <github@random.re>
2015-03-27 10:43:41 +0700

    updated for 3.1 password schema.

--- checkpassword-phpbb.py
+++ checkpassword-phpbb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 #
+# for phpBB 3.1
 # (C) Serge Victor, LGPLv3 license
 # https://github.com/ser/checkpassword-phpbb
 #
@@ -11,7 +12,7 @@ import pwd
 import sys
 import traceback
 import mysql.connector
-from passlib.hash import phpass # https://pythonhosted.org/passlib/lib/passlib.hash.phpass.html
+import bcrypt
 
 # phpBB MySQL details
 config = {
@@ -26,7 +27,7 @@ def verifypass(username,password):
     # Connect to the MySQL Server
     cnx = mysql.connector.connect(**config)
     # we want to ask mysql for password for particular phpBB user, 0=normal users, 3=forum owners (1=inactive, 2=bots)
-    sql = "SELECT user_password AS password FROM phpbb_users WHERE username_clean = '%s' AND user_type IN (0,3)" % username
+    sql = "SELECT user_password AS password FROM phpbb_users WHERE username_alias = '%s' AND user_type IN (0,3)" % username
     cursor = cnx.cursor(buffered=True)
     cursor.execute(sql)
     row = cursor.fetchone() # we need one record only
@@ -35,7 +36,11 @@ def verifypass(username,password):
     if row is None: # no such user, it cannot be authenticated   
        return False
     phpbbppasshash = bytes.decode(row[0]) 
-    return phpass.verify(password, phpbbppasshash) # True = user verified
+    phpbbppasshash = phpbbppasshash.encode('utf-8')
+    password = password.encode('utf-8')
+    if bcrypt.hashpw(password, phpbbppasshash) == phpbbppasshash:
+       return True
+    else: return False
 
 def main():
     scriptname = os.path.basename(sys.argv[0])
@@ -46,18 +51,20 @@ def main():
     try:
         domain = username.split("@")[1]
     except:
-        domain = "cia.gov.pl" # default domain, adapt it to your need
+        domain = "hyperre.al" # default domain, adapt it to your need
     if verifypass(justuser,password) == False:
        return 1 # password does not match, user cannot be authenticated
     else:
        os.environ['USER'] = "%s@%s" % (justuser, domain)
-       os.environ['HOME'] = "/home/vmail/%s/%s" % (domain, justuser) # adapt it to your needs
+       #os.environ['USER'] = "%s" % (justuser, )
+       os.environ['HOME'] = "/home/vmail/domains/%s/%s" % (domain, justuser) # adapt it to your needs
        os.environ['userdb_uid'] = "999" # adapt it to your needs
        os.environ['userdb_gid'] = "999" # adapt it to your needs
-       os.environ['userdb_mail'] = "maildir:~/Maildir" # adapt it to your needs
-       #os.environ['userdb_quota_rule'] = "*:storage=100M" # adapt it to your needs and add to EXTRA environ
+       # because the values are standard for all domains, I set it in conf.d/auth-sql.conf.ext
+       #os.environ['userdb_mail'] = "maildir:~/Maildir:INBOX=~/Maildir/.INBOX:INDEX=/home/vmail/indexes/%d/%n:CONTROL=/home/vmail/control/%d/%n" # adapt it to your needs
+       #os.environ['userdb_quota_rule'] = "*:storage=10000M" # adapt it to your needs and add to EXTRA environ
        os.environ['INSECURE_SETUID'] = "1"
-       os.environ['EXTRA'] = 'userdb_uid userdb_gid userdb_mail'
+       os.environ['EXTRA'] = 'userdb_uid userdb_gid'
     os.execvp(sys.argv[1], sys.argv[1:])
     # bye bye ;-)
 
